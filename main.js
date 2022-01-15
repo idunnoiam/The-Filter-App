15c0b7a6526ad5e84cb943daa8b621fcb12b802b
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2022-01-14 22:16:15 -0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX = 0;
+noseY = 0;
 
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
@@ -19,6 +22,7 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 80, 35)
 }
 
 function take_snapshot() {
@@ -28,8 +32,7 @@ function take_snapshot() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
-
+        noseX = results[0].pose.nose.x - 60;
+        noseY = results[0].pose.nose.y - 40;
     }
 }
